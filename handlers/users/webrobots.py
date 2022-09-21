b4c5c137cffdf8e3e21ff5595dcbd6fe008aca85
antaares <jrismoilov@gmail.com>
2022-09-21 22:14:48 +0500

    fix problems

--- handlers/users/webrobots.py
+++ handlers/users/webrobots.py
@@ -6,41 +6,29 @@ import asyncio
 
 @dp.message_handler(text="Web-saytlar")
 async def echo_web(message: types.Message):
-    base_message = await message.answer("@daho_dasturchi tamonidan yaratilgan saytlar ro'yxati:\n")
+    base_text = "@daho_dasturchi tamonidan yaratilgan web-saytlar ro'yxati:\n"
+    base_message = await message.answer(base_text)
     webs = db.get_webs()
     for web in webs:
         await asyncio.sleep(1)
-        base_text = base_message.text
-        n = "\n"
-        if web[0] == 0:
-            n = n*2
-        base_message = await base_message.edit_text(base_text + f"{n}{web[0] + 1} |💠| <a href=\"{web[2]}\">{web[1]}</a>")
+        base_text += f"\n{web[0] + 1} |💠| <a href='{web[2]}'>{web[1]}</a>"
+        base_message = await base_message.edit_text(base_text)
     await asyncio.sleep(1)
-    await base_message.edit_text(base_message.text + "\n\nYurishda davom etamiz!")
+    await base_message.edit_text(base_text + "\n\nYurishda davom etamiz!")
 
-
-# @dp.message_handler(text="Telegram bot")
-# async def echo_robot(message: types.Message):
-#     await message.answer("Telegram bot link")
 
 
 
 
 @dp.message_handler(text="Telegram bot")
 async def echo_robots(message: types.Message):
-    base_message = await message.answer("@daho_dasturchi tamonidan yaratilgan botlar ro'yxati:\n")
+    base_text = "@daho_dasturchi tamonidan yaratilgan botlar ro'yxati:\n"
+    base_message = await message.answer(base_text)
     robots = db.get_robots()
     for robot in robots:
         await asyncio.sleep(1)
-        base_text = base_message.text
-        n = "\n"
-        if robot[0] == 0:
-            n = n*2
-        base_message = await base_message.edit_text(base_text + f"{n}{robot[0] + 1} |💠| <a href=\"{robot[2]}\">{robot[1]}</a>")
+        base_text += f"\n{robot[0] + 1} |💠| <a href='{robot[2]}'>{robot[1]}</a>"
+        base_message = await base_message.edit_text(base_text)
     await asyncio.sleep(1)
-    await base_message.edit_text(base_message.text + "\n\nYurishda davom etamiz!")
-
-
-
-
+    await base_message.edit_text(base_text + "\n\nYurishda davom etamiz!")
 
